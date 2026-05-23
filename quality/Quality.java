3c2c91d349ca394ccfd03fdd459fb5035e224f4a
MinecraftFom <MinecraftFom477@gmail.com>
2026-05-23 15:12:34 +0800

    Added a small addition

--- quality/Quality.java
+++ quality/Quality.java
@@ -1,26 +1,34 @@
 package com.fomdev.awaken.quality;
 
+import com.fomdev.awaken.exp.EquipmentExperience;
 import com.fomdev.awaken.gen.GeneratingMobTypes;
+
+import java.awt.*;
 
 public interface Quality
 {
     String id();
+    Color color();
 
     boolean              infectedOnly();
 
     float                chance();
-    int                  durability();
+    float                durability();
     int                  enchant();
     float                factor();
     int                  level();
     GeneratingMobTypes[] mobs();
 
+    default int maxUpgradeLevel() { return EquipmentExperience.defaultInitialExperienceRequirement; }
+    default float upgradeFactor() { return EquipmentExperience.defaultMaxExperienceFactor; }
+
     static Quality of
             (
                     String id,
+                    Color color,
                     boolean infected,
                     float chance,
-                    int durability,
+                    float durability,
                     int enchant,
                     float factor,
                     int level,
@@ -42,13 +50,19 @@ public interface Quality
             }
 
             @Override
+            public Color color()
+            {
+                return color;
+            }
+
+            @Override
             public float chance()
             {
                 return chance;
             }
 
             @Override
-            public int durability()
+            public float durability()
             {
                 return durability;
             }
@@ -75,6 +89,91 @@ public interface Quality
             public GeneratingMobTypes[] mobs()
             {
                 return mobs;
+            }
+        };
+    }
+
+    static Quality of
+            (
+                    String id,
+                    Color color,
+                    boolean infected,
+                    float chance,
+                    float durability,
+                    int enchant,
+                    float factor,
+                    int level,
+                    GeneratingMobTypes[] mobs,
+                    int maxUpgradeLevel,
+                    float upgradeFactor
+            )
+    {
+        return new Quality()
+        {
+            @Override
+            public boolean infectedOnly()
+            {
+                return infected;
+            }
+
+            @Override
+            public String id()
+            {
+                return id;
+            }
+
+            @Override
+            public Color color()
+            {
+                return color;
+            }
+
+            @Override
+            public float chance()
+            {
+                return chance;
+            }
+
+            @Override
+            public float durability()
+            {
+                return durability;
+            }
+
+            @Override
+            public int enchant()
+            {
+                return enchant;
+            }
+
+            @Override
+            public float factor()
+            {
+                return factor;
+            }
+
+            @Override
+            public int level()
+            {
+                return level;
+            }
+
+            @Override
+            public GeneratingMobTypes[] mobs()
+            {
+                return mobs;
+            }
+
+            @Override
+            public int maxUpgradeLevel()
+            {
+                return maxUpgradeLevel;
+            }
+
+            @Override
+            public float upgradeFactor()
+            {
+                return upgradeFactor;
             }
         };
     }
--- quality/QualityUtil.java
+++ quality/QualityUtil.java
@@ -1,9 +1,12 @@
 package com.fomdev.awaken.quality;
 
+import com.fomdev.awaken.gen.GeneratingMobTypes;
+import com.fomdev.awaken.init.AwakenRPG;
 import com.fomdev.flib.util.Suggested;
 import net.minecraft.resources.ResourceLocation;
 
 import javax.annotation.Nullable;
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
@@ -11,6 +14,24 @@ import java.util.concurrent.atomic.AtomicReference;
 public class QualityUtil
 {
     private static final Map<ResourceLocation, Quality> registeredQualities = new HashMap<>();
+
+    public static final Quality naive;
+    public static final Quality novice;
+//    public static final Quality prehistoric;
+//    public static final Quality prelithic;
+//    public static final Quality basic;
+//    public static final Quality started;
+//    public static final Quality learner;
+//    public static final Quality smiths;
+//    public static final Quality ancient;
+//    public static final Quality pizzazz;
+//    public static final Quality symmetrical;
+//    public static final Quality mysterious;
+//    public static final Quality virtual;
+//    public static final Quality tremendous;
+//    public static final Quality masterpiece;
+//    public static final Quality legendary;
+    public static final Quality infinity;
 
     @Nullable
     public static ResourceLocation getQualityId(
@@ -43,6 +64,13 @@ public class QualityUtil
         return getQuality(ResourceLocation.fromNamespaceAndPath(modid, id));
     }
 
+    public static String localize(
+            String id
+    )
+    {
+        return "quality."+id+".name";
+    }
+
     public static Quality registerQuality(
             String modid,
             Quality quality
@@ -53,5 +81,55 @@ public class QualityUtil
             throw new IllegalArgumentException("Invalid register id: " + location + ", already registered");
 
         return registeredQualities.put(location, quality);
+    }
+
+    static
+    {
+        naive = registerQuality(
+                AwakenRPG.MODID,
+                Quality.of(
+                        "naive",
+                        new Color(0xAA, 0xAA, 0xAA),
+                        false,
+                        0.7F,
+                        0.1F,
+                        5,
+                        0.01F,
+                        1,
+                        new GeneratingMobTypes[]{GeneratingMobTypes.Types.MONSTER},
+                        3000,
+                        6.0F
+                )
+        );
+
+        novice = registerQuality(
+                AwakenRPG.MODID,
+                Quality.of(
+                        "novice",
+                        new Color(0xAB, 0xAB, 0xAB),
+                        false,
+                        0.67F,
+                        0.1F,
+                        6,
+                        0.012F,
+                        2,
+                        new GeneratingMobTypes[]{GeneratingMobTypes.Types.MONSTER}
+                )
+        );
+
+        infinity = registerQuality(
+                AwakenRPG.MODID,
+                Quality.of(
+                        "infinity",
+                        new Color(0xFF, 0xFE, 0xFE, 0xA),
+                        true,
+                        0.00001F,
+                        0.5F,
+                        255,
+                        20F,
+                        17,
+                        new GeneratingMobTypes[]{GeneratingMobTypes.Types.BOSS}
+                )
+        );
     }
 }
