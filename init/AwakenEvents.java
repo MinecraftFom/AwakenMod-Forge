6ffa3815777bab9ecb4acfff1a321304959bdfd5
MinecraftFom <MinecraftFom477@gmail.com>
2026-05-25 22:50:24 +0800

    Added a dev test event

--- init/AwakenEvents.java
+++ init/AwakenEvents.java
@@ -7,8 +7,10 @@ import com.fomdev.awaken.nbt.NBTUtil;
 import com.fomdev.awaken.register.item.FunctionalItems;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -16,15 +18,50 @@ import net.minecraftforge.fml.common.Mod;
 public class AwakenEvents
 {
     @SubscribeEvent
-    public static void onLevelDetectorRun(PlayerInteractEvent.RightClickEmpty event)
+    public static void onLevelDetectorRun(ItemTossEvent event)
     {
-        ItemStack stack = event.getItemStack();
-        AwakenLevel level = AwakenLevelRegister.getLevel(NBTUtil.deserializeAwakenLevel(event.getEntity()));
+        ItemStack stack = event.getEntity().getItem();
+        AwakenLevel level = AwakenLevelRegister.getLevel(NBTUtil.deserializeAwakenLevel(event.getPlayer()));
 
-//        if (stack.getItem() == FunctionalItems.AWAKEN_LEVEL_DETECTOR.get())
-//        {
-//            if (level != null) event.getEntity().sendSystemMessage(Component.translatable("chat.current_awaken_level.msg").append(Component.translatable(AwakenLevelManager.localize(level.id()))).append(Component.literal(" [" + NBTUtil.deserializeAwakenLevel(event.getEntity())+ "]")).withStyle(ChatFormatting.GREEN));
-//            else event.getEntity().sendSystemMessage(Component.translatable("chat.current_awaken_level.err"));
-//        }
+        if (stack.getItem() == FunctionalItems.AWAKEN_LEVEL_DETECTOR.get() && stack.getCount() == 1)
+        {
+            if (level != null)
+            {
+                float lvl = NBTUtil.deserializeAwakenLevel(event.getPlayer());
+
+                event.getPlayer().sendSystemMessage(
+                        Component
+                                .translatable("chat.current_awaken_level.msg")
+                                .append(
+                                        Component
+                                                .translatable(AwakenLevelManager.localize(level.id())
+                                                )
+                                )
+                                .append(
+                                        Component
+                                                .literal(" [" + lvl + "]")
+                                )
+                                .withStyle(ChatFormatting.GREEN));
+            }
+            else event.getPlayer().sendSystemMessage(Component.translatable("chat.current_awaken_level.err"));
+
+            event.cancel();
+            event.getPlayer().addItem(stack);
+        }
+    }
+
+    @SubscribeEvent
+    public static void listenAwakenCommand(ServerChatEvent event)
+    {
+        Player player = event.getPlayer();
+        String message = event.getMessage().getString();
+
+        if (message.startsWith("!!awaken") && player.getName().getString().equals("Dev")) // Only Dev can use this
+        {
+            String num = message.split(" ")[1];
+            float fl_num = Float.parseFloat(num);
+
+            AwakenLevelManager.awaken(player, fl_num, 0);
+        }
     }
 }
