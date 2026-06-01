7489aa43fa79b4dc805f4d57a9ea5fc0cfcff2a2
MinecraftFom <MinecraftFom477@gmail.com>
2026-06-01 19:03:23 +0800

    Fixed difficulty handler

--- gen/DifficultyHandler.java
+++ gen/DifficultyHandler.java
@@ -1,8 +1,9 @@
 package com.fomdev.awaken.gen;
 
 import com.fomdev.awaken.init.Awaken;
+import com.fomdev.awaken.nbt.NBTUtil;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.level.saveddata.SavedData;
@@ -15,12 +16,54 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @Mod.EventBusSubscriber(modid = Awaken.MODID)
 public class DifficultyHandler
 {
+    public static final Random random = new Random(System.currentTimeMillis());
     public static final String namespace = "awakenedDifficulty";
     public static final Map<ServerLevel, SavedDifficultyLevel> levels = new HashMap<>();
+
+    public static final Map<ResourceLocation, Float> dimensionFactor = new HashMap<>();
+
+    public static float getLevelDifficulty(
+            ServerLevel level
+    )
+    {
+        if (!levels.containsKey(level))
+            return 0.0F;
+
+        SavedDifficultyLevel lvl = levels.get(level);
+        if (lvl == null)
+            return 0.0F;
+
+        return lvl.getLevel();
+    }
+
+    public static void registerDimensionFactor(
+            ResourceLocation dimensionID,
+            Float factor
+    )
+    {
+        if (dimensionFactor.containsKey(dimensionID))
+            return;
+
+        dimensionFactor.put(dimensionID, factor);
+    }
+
+    public static void setLevelDifficulty(
+            ServerLevel level,
+            float diff
+    )
+    {
+        if (!levels.containsKey(level))
+            levels.put(level, readDifficulty(level));
+
+        SavedDifficultyLevel sl = levels.get(level);
+        if (sl != null)
+            sl.setLevel(diff);
+    }
 
     @SubscribeEvent
     public static void onLevelDayPassed(
@@ -33,6 +76,9 @@ public class DifficultyHandler
         if (level.getDayTime() % 24000 == 0) // A new day
         {
             int day = Math.toIntExact(level.getDayTime() / 24000);
+            if (day == 0)
+                return; // The first day has no difficulty
+
             Difficulty diff = level.getDifficulty();
 
             float diffFactor = switch (diff)
@@ -43,9 +89,33 @@ public class DifficultyHandler
                 case HARD -> 20.0F;
             };
 
-            int playerCount = level.players().size();
+            int playerCount = level.getServer().getPlayerList().getPlayerCount();
+            if (playerCount == 0)
+                return;
 
+            Float[] levels = level.getServer().getPlayerList().getPlayers().stream().map(NBTUtil::deserializeAwakenLevel).toArray(Float[]::new);
+            float total = 0.0F;
+            for (Float fl: levels) total += fl;
+            float average = total / playerCount;
+            float offset = day * diffFactor;
 
+            if (average == 0)
+                average = 0.01F;
+
+            if (offset == 0)
+                offset = 0.01F;
+
+            float diffOffset = random.nextFloat(offset);
+            if (diffOffset == 0)
+                diffOffset = 0.01F;
+
+            float randValue = random.nextFloat(diffOffset * average) / day;
+            if (randValue == 0)
+                randValue = 0.01F;
+
+            float currentDifficulty = getLevelDifficulty(level);
+            float dimedValue = randValue * getDimensionFactor(level);
+            setLevelDifficulty(level, currentDifficulty + dimedValue * 100);
         }
     }
 
@@ -68,7 +138,9 @@ public class DifficultyHandler
         if (!(event.getLevel() instanceof ServerLevel serverLevel))
             return;
 
-        levels.get(serverLevel).setDirty();
+        SavedDifficultyLevel level = levels.get(serverLevel);
+        if (level != null)
+            level.setDirty();
     }
 
     public static SavedDifficultyLevel readDifficulty(
@@ -84,38 +156,70 @@ public class DifficultyHandler
         );
     }
 
+    private static float getDimensionFactor(
+            ServerLevel level
+    )
+    {
+        ResourceLocation location = level.dimensionTypeId().location();
+
+        if (!dimensionFactor.containsKey(location))
+            return 1.0F;
+
+        Float factor = dimensionFactor.get(location);
+        if (factor == null)
+            return 1.0F;
+
+        return factor;
+    }
+
     public static class SavedDifficultyLevel extends SavedData
     {
-        private int level = 0;
+        private float level = 0.0F;
 
-        private SavedDifficultyLevel() {}
+        public SavedDifficultyLevel() {}
 
         public static SavedDifficultyLevel load(CompoundTag tag)
         {
-            if (!tag.contains(namespace))
-                tag.putInt(namespace, 0);
-
             SavedDifficultyLevel data = new SavedDifficultyLevel();
-            data.level = tag.getInt(namespace);
+            data.level = tag.contains(namespace)? tag.getFloat(namespace): 0.0F;
 
             return data;
         }
 
-        public int getLevel()
+        public float getLevel()
         {
             return this.level;
         }
 
-        public void setLevel(int level)
+        public void setLevel(float level)
         {
             this.level = level;
+            this.setDirty();
         }
 
         @Override
         public @NotNull CompoundTag save(@NotNull CompoundTag tag)
         {
-            tag.putInt(namespace, level);
+            tag.putFloat(namespace, this.level);
             return tag;
         }
+    }
+
+    static
+    {
+        registerDimensionFactor(
+                ResourceLocation.parse("minecraft:overworld"),
+                0.45F
+        );
+
+        registerDimensionFactor(
+                ResourceLocation.parse("minecraft:the_nether"),
+                1.25F
+        );
+
+        registerDimensionFactor(
+                ResourceLocation.parse("minecraft:the_end"),
+                3.24F
+        );
     }
 }
